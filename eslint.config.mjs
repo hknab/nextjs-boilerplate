ab6fddbab1b334f03b0faf4d08bd7d55ed49d20f
hkn <hakan.ab02@gmail.com>
2024-12-24 19:00:25 +0300

    chore(linters): add style and git linters
    add eslint prettier commit linter and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    plugins: {
+      prettier,
+    },
+  },
+  {
+    files: ['./src/**/*.ts', './src/**/*.tsx', '**/*.d.ts'],
+  },
 ];
-
-export default eslintConfig;
+export default config;
