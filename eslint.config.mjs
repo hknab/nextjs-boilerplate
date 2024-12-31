654ca70a07e323376e3d604ed57229103cff1df4
hkn <hakan.ab02@gmail.com>
2024-12-31 12:10:28 +0300

    feat(jest): added initial setup for testing framework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import jest from 'eslint-plugin-jest';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,15 +19,20 @@ const config = [
     'eslint:recommended',
     'plugin:@next/next/recommended',
     'plugin:prettier/recommended',
-    'plugin:storybook/recommended'
+    'plugin:storybook/recommended',
+    'plugin:jest/recommended'
   ),
   {
     plugins: {
       prettier,
+      jest,
     },
   },
   {
     files: ['./src/**/*.ts', './src/**/*.tsx', '**/*.d.ts'],
+    env: {
+      jest: true, // Add Jest environment
+    },
   },
 ];
 export default config;
